4d37db686fd46630fd1178919d1bb18d9c5ae24d
rhard32 <rhard32@gmail.com>
2015-04-16 11:02:12 -0400

    Created failed test for Name

---
+++ TDDLab2/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDLab2
+{
+    public class personName
+    {
+        private string p;
+
+        public personName(string p)
+        {
+            // TODO: Complete member initialization
+            this.p = p;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ TDDLab2Test/TDDLab2Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TDDLab2;
+
+namespace TDDLab2Test1
+{ 
+
+    public class Name
+    {
+        [Fact]
+        public void TddLab2Test()
+    {
+        var personTestName = new personName("John Doe");
+        Assert.Equal("John Doe", personTestName.Name );
+    }
+      
+    }
+}
